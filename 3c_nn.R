499ce91d471f4e8bc3b0a8b126faffe0ff8c4992
akane2460 <allisonkane2024@u.northwestern.edu>
2024-02-28 17:32:15 -0600

    Renaming nearest neighbors

--- 3c_nn.R
+++ 3c_nn.R
@@ -1,5 +1,5 @@
 # L05 Resampling ----
-# Define and fit ridge
+# Define and fit nearest neighbor
 
 # load packages ----
 library(tidyverse)
@@ -15,20 +15,26 @@ load(here("data/diabetic_fold.rda"))
 # load pre-processing/feature engineering/recipe
 load(here("recipes/diabetic_recipe_lm.rda"))
 
+set.seed(2201946)
+
 # model specifications ----
-log_reg_spec <- 
-  logistic_reg() |> 
-  set_engine("glm") |> 
-  set_mode("classification") 
+# A nearest neighbor (`nearest_neighbor()`) with the `kknn` engine setting and `neighbors` set to 20.
+knn_spec <- 
+  nearest_neighbor(neighbors = 10) |> 
+  set_engine("kknn") |> 
+  set_mode("classification")
+
+# knn_spec <- nearest_neighbor(mode = "classification", neighbors = 10) |> 
+#   set_engine("kknn")
 
 # define workflows ----
-log_reg_wflow <-
+knn_wflow <-
   workflow() |> 
-  add_model(log_reg_spec) |> 
+  add_model(knn_spec) |> 
   add_recipe(diabetic_recipe_lm)
 
 # fit workflows/models ----
-fit_log_reg <- fit(log_reg_wflow, diabetic_fold)
+diabetic_fit_nn <- fit_resamples(knn_wflow, diabetic_fold)
 
 # save fit
 # save(diabetic_null_fit, file = here("results/diabetic_null_fit.rda"))
