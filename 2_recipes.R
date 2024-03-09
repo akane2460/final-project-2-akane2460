6ae71bd6faf2ef85ff67d40a6f6b1e55754c8814
akane2460 <allisonkane2024@u.northwestern.edu>
2024-03-08 23:41:44 -0600

    adjusting recipes

--- 2_recipes.R
+++ 2_recipes.R
@@ -21,26 +21,29 @@ load(here("data/diabetic_test.rda"))
 # recipe a: simple recipe----
 
 # description of what is in the recipe
-    # for initial recipe, the general category change will be examined as a placeholder
+    # for initial recipe, the general category change will be examined as a more general predictor
       # for the more specific medication variables like metformin or insulin (etc)
       # the change variable encompasses if there is any change in ANY diabetes medication
       # whereas these specific medication variables measure if that medication
-      # changed in its prescription. 
+      # changed in its prescription.
       # in the future, accounting for the different effects of different drugs
       # might be worthwhile. but for now we are examining overall changes in diabetes medication
-    
+    # basic test results max_glu_serum and a1cresult are examined as well
     # basic demographics (age, race, gender) and their lab results (max_glu_serum and a1cresult)
     # are important assessments of their risk for certain complications of diabetes
-    
+
     # the number of procedures, doctor visits, days in the hospital, etc. are all indicators
     # of how severe their side effects or complications of diabetes could be
 
 # recipe
 null_diabetic_recipe <- recipe(
   readmitted ~ age + race + gender + max_glu_serum + a1cresult
-  + change + num_lab_procedures + num_procedures + num_medications + number_diagnoses +
-    number_emergency + time_in_hospital + number_outpatient + number_inpatient,
+  + change + num_lab_procedures + num_procedures + num_medications + number_diagnoses + 
+    time_in_hospital + number_outpatient + number_inpatient,
   data = diabetic_train) |> 
+  step_nzv(all_predictors()) |>  
+  step_center(all_numeric_predictors()) |>  
+  step_scale(all_numeric_predictors()) |> 
   step_dummy(all_nominal_predictors(), one_hot = TRUE)
       # additional interactions might be considered in future
 
@@ -53,32 +56,61 @@ null_diabetic_recipe <- recipe(
 # save recipe
 save(null_diabetic_recipe, file = here("recipes/null_diabetic_recipe.rda"))
 
-# recipe b: lasso----
-lasso_recipe <- recipe(
-  readmitted ~ age + race + gender + max_glu_serum + a1cresult
-  + change + num_lab_procedures + num_procedures + num_medications + number_diagnoses +
-    number_emergency + time_in_hospital + number_outpatient + number_inpatient,
+# recipe b: feature engineered basic recipe----
+featured_recipe <- recipe(
+  readmitted ~ age + race + gender + max_glu_serum + a1cresult + 
+    change + num_lab_procedures + num_procedures + num_medications + number_diagnoses + 
+    time_in_hospital + number_outpatient + number_inpatient,
   data = diabetic_train) |> 
   step_nzv(all_predictors()) |>  
   step_center(all_numeric_predictors()) |>  
-  step_scale(all_numeric_predictors()) |> 
-  step_dummy(all_nominal_predictors(), one_hot = FALSE) 
+  step_scale(all_numeric_predictors()) |>
+  step_interact(~ age : c(number_diagnoses, time_in_hospital, number_inpatient)) |>
+  step_interact(~ gender : time_in_hospital) |> 
+  step_interact(~ time_in_hospital : c(num_medications, num_lab_procedures)) |>
+  step_interact(~ num_procedures : num_medications) |> 
+  step_interact(~ race : number_diagnoses) |> 
+  step_interact(~ a1cresult : num_lab_procedures) |>
+  step_dummy(all_nominal_predictors(), one_hot = TRUE)
+
+
+# testing recipe
+featured_recipe |>
+  prep() |>
+  bake(new_data = NULL) |>
+  glimpse()
 
 # save recipe
-save(lasso_recipe, file = here("recipes/lasso_recipe.rda"))
+save(featured_recipe, file = here("recipes/featured_recipe.rda"))
 
-# recipe c: tree based----
-tree_based_diabetic_recipe <- recipe(
+# recipe c: advanced, featured recipe----
+
+# this recipe accounts for specific drugs (rather than Change)
+
+advanced_recipe <- recipe(
   readmitted ~ age + race + gender + max_glu_serum + a1cresult
-  + change + num_lab_procedures + num_procedures + num_medications + number_diagnoses +
-    number_emergency + time_in_hospital + number_outpatient + number_inpatient,
+  + metformin + insulin + metformin + num_lab_procedures + num_procedures + 
+    num_medications + number_diagnoses + time_in_hospital + number_outpatient 
+  + number_inpatient + rosiglitazone + pioglitazone + glyburide + glipizide + glimepiride,
   data = diabetic_train) |> 
-  step_interact(~ time_in_hospital : c(num_lab_procedures, number_diagnoses, num_procedures)) |> 
-  # with more time in the hospital, typically doing more tests/procedures and 
-  # providing more diagnoses
-  step_interact(~ number_emergency : c(number_outpatient, number_inpatient))
-# if you have a medical emergency, typically you will receive either outpatient or inpatient
-# medical care
-  
+  step_nzv(all_predictors()) |>  
+  step_center(all_numeric_predictors()) |>  
+  step_scale(all_numeric_predictors()) |>
+  step_interact(~ age : c(number_diagnoses, time_in_hospital, number_inpatient)) |>
+  step_interact(~ gender : time_in_hospital) |> 
+  step_interact(~ time_in_hospital : c(num_medications, num_lab_procedures)) |>
+  step_interact(~ num_procedures : num_medications) |> 
+  step_interact(~ race : number_diagnoses) |> 
+  step_interact(~ a1cresult : num_lab_procedures) |>
+  step_interact(~ insulin : num_lab_procedures) |>
+  step_interact(~ metformin : num_lab_procedures) |>
+  step_dummy(all_nominal_predictors(), one_hot = TRUE)
 
-save(tree_based_diabetic_recipe, file = here("recipes/tree_based_diabetic_recipe.rda"))
+# testing recipe
+advanced_recipe |>
+  prep() |>
+  bake(new_data = NULL) |>
+  glimpse()
+
+# save recipe
+save(advanced_recipe, file = here("recipes/advanced_recipe.rda"))
