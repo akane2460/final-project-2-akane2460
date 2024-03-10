86e981e815b8f137d92ed42e96ce52d66dac43e9
akane2460 <allisonkane2024@u.northwestern.edu>
2024-03-09 20:16:34 -0600

    knn script

--- 3e_knn.R
+++ 3e_knn.R
@@ -23,10 +23,11 @@ registerDoMC(cores = num_cores)
 
 # load training data
 load(here("data/diabetic_fold.rda"))
-load(here("data/diabetic_train.rda"))
 
 # load pre-processing/feature engineering/recipe
 load(here("recipes/null_diabetic_recipe.rda"))
+load(here("recipes/featured_recipe.rda"))
+load(here("recipes/advanced_recipe.rda"))
 
 # set seed
 set.seed(907012463)
@@ -38,34 +39,85 @@ kknn_spec <-
   set_mode("classification")
 
 # define workflows ----
-kknn_model <-
+null_knn_wflow <-
   workflow() |> 
   add_model(kknn_spec) |> 
   add_recipe(null_diabetic_recipe)
 
+# featured wflow
+featured_knn_wflow <-
+  workflow() |> 
+  add_model(kknn_spec) |> 
+  add_recipe(featured_recipe)
+
+# advanced wflow
+advanced_knn_wflow <-
+  workflow() |> 
+  add_model(kknn_spec) |> 
+  add_recipe(advanced_recipe)
+
 # hyperparameter tuning values ----
+# null hyperparameters
+hardhat::extract_parameter_set_dials(null_knn_wflow)
 
-# check ranges for hyperparameters
-hardhat::extract_parameter_set_dials(kknn_model)
+null_knn_params <- parameters(null_knn_wflow) |>  
+  update(neighbors = neighbors())
 
-# change hyperparameter ranges
-kknn_params <- parameters(kknn_model)
+null_knn_grid <- grid_regular(null_knn_params, levels = 8)
 
-# build tuning grid
-kknn_grid <- grid_regular(kknn_params, levels = 5)
+# featured hyperparameters
+hardhat::extract_parameter_set_dials(featured_knn_wflow)
+
+featured_knn_params <- parameters(featured_knn_wflow) |>  
+  update(neighbors = neighbors())
+
+featured_knn_grid <- grid_regular(featured_knn_params, levels = 8)
+
+# advanced hyperparameters
+hardhat::extract_parameter_set_dials(advanced_knn_wflow)
+
+advanced_knn_params <- parameters(advanced_knn_wflow) |>  
+  update(neighbors = neighbors())
+
+advanced_knn_grid <- grid_regular(advanced_knn_params, levels = 8)
 
 # fit workflows/models ----
+# null
 # set seed
-set.seed(29874223)
-
-kknn_tuned <- 
-  kknn_model |> 
+set.seed(02917023)
+null_knn_tuned <- 
+  null_knn_wflow |> 
   tune_grid(
     diabetic_fold, 
-    grid = kknn_grid, 
+    grid = null_knn_grid, 
     control = control_grid(save_workflow = TRUE)
   )
 
+# featured
+# set seed
+set.seed(021947965)
+featured_knn_tuned <- 
+  featured_knn_wflow |> 
+  tune_grid(
+    diabetic_fold, 
+    grid = featured_knn_grid, 
+    control = control_grid(save_workflow = TRUE)
+  )
+
+
+# advanced
+set.seed(64912780)
+advanced_knn_tuned <- 
+  advanced_knn_wflow |> 
+  tune_grid(
+    diabetic_fold, 
+    grid = advanced_knn_grid, 
+    control = control_grid(save_workflow = TRUE)
+  )
+
+
 # save fit
-save(diabetic_fit_nn, file = here("results/diabetic_fit_nn.rda"))
+save(null_knn_tuned, file = here("results/null_knn_tuned.rda"))
+save(featured_knn_tuned, file = here("results/featured_knn_tuned.rda"))
+save(advanced_knn_tuned, file = here("results/advanced_knn_tuned.rda"))
 
