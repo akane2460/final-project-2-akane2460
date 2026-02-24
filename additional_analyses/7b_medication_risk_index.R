07ffd3c57edcb79c70f7429694b54473b7df3d09
akane2460 <allisonkane2024@u.northwestern.edu>
2026-02-24 16:39:13 -0600

    updated double risk plots

--- additional_analyses/7b_medication_risk_index.R
+++ additional_analyses/7b_medication_risk_index.R
@@ -374,159 +374,6 @@ metformin_rosiglitazone <- med_risk_grid_fxn(double_meds_df, "metformin", "rosig
     # metformin x med red flags:
         #metformin x glyburide: both changed incr risk ()
 
-cols <- c("Changed × Changed" = "#8DCE8D", "One Changed" = "#4CB04C", "Steady × Steady" = "#317231")
-
-# insulin x glyburide
-insulin_x_glyburide_readmit_plot <- diabetic_clean |>
-  filter(insulin != "No", glyburide!= "No") |>
-    mutate(
-      insulin_changed = insulin != "Steady",
-      glyburide_changed = glyburide != "Steady",
-      instability_tier = case_when(
-        !insulin_changed & !glyburide_changed ~ "Steady × Steady",
-        insulin_changed & glyburide_changed ~ "Changed × Changed",
-        TRUE ~ "One Changed"
-      ) ) |> 
-  group_by(instability_tier) |>
-    summarize(
-    readmit_rate = mean(readmitted == "YES", na.rm = TRUE),
-    n = n(),
-    .groups = "drop"
-  ) |> 
-  ggplot(aes(x = instability_tier,
-            y = readmit_rate,
-            fill = instability_tier)) +
-  geom_col(position = "dodge") +
-  geom_text(
-    aes(label = scales::percent(readmit_rate, accuracy = 0.1)),
-    vjust = -0.5,
-    size = 4
-  ) +
-  scale_fill_manual(values = cols, guide = "none") +
-  scale_y_continuous(labels = scales::percent) +
-  labs(
-    title = "Readmission Rate by Insulin × Glyburide Regimen Stability",
-    x = "Regimens",
-    y = "30-Day Readmission Rate"
-  ) +
-  theme_minimal()
-
-ggsave("additional_analyses/plots/insulin_x_glyburide_readmit_plot.png", plot = insulin_x_glyburide_readmit_plot, width = 6.5, height = 8)
-
-  
-# insulin x glimepiride
-insulin_x_glimepiride_readmit_plot <- diabetic_clean |>
-  filter(insulin != "No", glimepiride != "No") |>
-  mutate(
-    insulin_changed = insulin != "Steady",
-    glimepiride_changed = glimepiride != "Steady",
-    instability_tier = case_when(
-      !insulin_changed & !glimepiride_changed ~ "Steady × Steady",
-      insulin_changed & glimepiride_changed ~ "Changed × Changed",
-      TRUE ~ "One Changed"
-    ) ) |> 
-  group_by(instability_tier) |>
-  summarize(
-    readmit_rate = mean(readmitted == "YES", na.rm = TRUE),
-    n = n(),
-    .groups = "drop"
-  ) |> 
-  ggplot(aes(x = instability_tier,
-             y = readmit_rate,
-             fill = instability_tier)) +
-  geom_col(position = "dodge") +
-  geom_text(
-    aes(label = scales::percent(readmit_rate, accuracy = 0.1)),
-    vjust = -0.5,
-    size = 4
-  ) +
-  scale_fill_manual(values = cols, guide = "none") +
-  scale_y_continuous(labels = scales::percent) +
-  labs(
-    title = "Readmission Rate by Insulin × Glimepiride Regimen Stability",
-    x = "Regimens",
-    y = "30-Day Readmission Rate"
-  ) +
-  theme_minimal()
-
-ggsave("additional_analyses/plots/insulin_x_glimepiride_readmit_plot.png", plot = insulin_x_glimepiride_readmit_plot, width = 6.5, height = 8)
-
-# insulin x metformin
-insulin_x_metformin_readmit_plot <- diabetic_clean |>
-  filter(insulin != "No", metformin != "No") |>
-  mutate(
-    insulin_changed = insulin != "Steady",
-    metformin_changed = metformin != "Steady",
-    instability_tier = case_when(
-      !insulin_changed & !metformin_changed ~ "Steady × Steady",
-      insulin_changed & metformin_changed ~ "Changed × Changed",
-      TRUE ~ "One Changed"
-    ) ) |> 
-  group_by(instability_tier) |>
-  summarize(
-    readmit_rate = mean(readmitted == "YES", na.rm = TRUE),
-    n = n(),
-    .groups = "drop"
-  ) |> 
-  ggplot(aes(x = instability_tier,
-             y = readmit_rate,
-             fill = instability_tier)) +
-  geom_col(position = "dodge") +
-  geom_text(
-    aes(label = scales::percent(readmit_rate, accuracy = 0.1)),
-    vjust = -0.5,
-    size = 4
-  ) +
-  scale_fill_manual(values = cols, guide = "none") +
-  scale_y_continuous(labels = scales::percent) +
-  labs(
-    title = "Readmission Rate by Insulin × Metformin Regimen Stability",
-    x = "Regimens",
-    y = "30-Day Readmission Rate"
-  ) +
-  theme_minimal()
-
-ggsave("additional_analyses/plots/insulin_x_metformin_readmit_plot.png", plot = insulin_x_metformin_readmit_plot, width = 6.5, height = 8)
-
-
-# metformin x glyburide
-metformin_x_glyburide_readmit_plot <- diabetic_clean |>
-  filter(glyburide != "No", metformin != "No") |>
-  mutate(
-    glyburide_changed = glyburide != "Steady",
-    metformin_changed = metformin != "Steady",
-    instability_tier = case_when(
-      !glyburide_changed & !metformin_changed ~ "Steady × Steady",
-      glyburide_changed & metformin_changed ~ "Changed × Changed",
-      TRUE ~ "One Changed"
-    ) ) |> 
-  group_by(instability_tier) |>
-  summarize(
-    readmit_rate = mean(readmitted == "YES", na.rm = TRUE),
-    n = n(),
-    .groups = "drop"
-  ) |> 
-  ggplot(aes(x = instability_tier,
-             y = readmit_rate,
-             fill = instability_tier)) +
-  geom_col(position = "dodge") +
-  geom_text(
-    aes(label = scales::percent(readmit_rate, accuracy = 0.1)),
-    vjust = -0.5,
-    size = 4
-  ) +
-  scale_fill_manual(values = cols, guide = "none") +
-  scale_y_continuous(labels = scales::percent) +
-  labs(
-    title = "Readmission Rate by Glyburide × Metformin Regimen Stability",
-    x = "Regimens",
-    y = "30-Day Readmission Rate"
-  ) +
-  theme_minimal()
-
-ggsave("additional_analyses/plots/metformin_x_glyburide_readmit_plot.png", plot = metformin_x_glyburide_readmit_plot, width = 6.5, height = 8)
-
-
 # Building a Medication Instability Index----
 
 # MII
@@ -569,15 +416,26 @@ single_med_red_flags <- single_med_red_flags  |>
     # metformin x med red flags:
     #metformin x glyburide: both changed incr risk ()
 
-medication_1 <- c("insulin", "insulin", "insulin", "metformin")
-medication_2 <- c("glyburide", "glimepiride", "metformin", "glyburide")
+# medication_1 <- c("insulin", "insulin", "insulin", "metformin")
+# medication_2 <- c("glyburide", "glimepiride", "metformin", "glyburide")
+# regimen_1 <- c("Changed", "Steady", "Changed", "Changed")
+# regimen_2 <- c("Changed", "Steady", "Changed", "Changed")
+
+medication_1 <- c("insulin", "metformin")
+medication_2 <- c("metformin", "glyburide")
 regimen_1 <- c("Changed", "Steady", "Changed", "Changed")
 regimen_2 <- c("Changed", "Steady", "Changed", "Changed")
 
+# interaction_matrices <- list(
+#   insulin_glyburide    = insulin_glyburide,
+#   insulin_glimepiride  = insulin_glimepiride,
+#   insulin_metformin   = insulin_metformin,
+#   metformin_glyburide = metformin_glyburide
+# )
 
 interaction_matrices <- list(
-  insulin_glyburide    = insulin_glyburide,
-  insulin_glimepiride  = insulin_glimepiride,
+  # insulin_glyburide    = insulin_glyburide,
+  # insulin_glimepiride  = insulin_glimepiride,
   insulin_metformin   = insulin_metformin,
   metformin_glyburide = metformin_glyburide
 )
@@ -630,35 +488,274 @@ double_med_red_flags <- double_med_red_flags |>
 
 
 
-diabetic_clean |>
-  filter(insulin != "No", glimepiride != "No") |>
+# insulin_metformin plot
+cols <- c("Both Changed" = "#8DCE8D", "Metformin Changed" = "#4CB04C", "Insulin Changed" = "#317231", "Both Steady" = "#204D20")
+
+insulin_metformin <- as.data.frame(interaction_matrices$insulin_metformin)
+
+insulin_x_metformin_readmit_plot <- insulin_metformin |>
   mutate(
-    insulin_collapsed = if_else(insulin == "Steady", "Steady", "Changed"),
-    glimepiride_collapsed = if_else(glimepiride == "Steady", "Steady", "Changed")
-  ) |> 
-  group_by(insulin_collapsed, glimepiride_collapsed) |>
-  summarize(
-    readmit_rate = mean(readmitted == "YES", na.rm = TRUE),
-    n = n(),
-    .groups = "drop"
-  ) |> 
-  filter(insulin_collapsed == 'Changed' & glimepiride_collapsed == 'Changed' | insulin_collapsed == 'Steady' & glimepiride_collapsed == 'Steady') |> 
-  ggplot( aes(x = insulin_collapsed,
-              y = readmit_rate)) +
+    instability_tier = case_when(
+      insulin_status == 'Changed' & metformin_status == 'Changed' ~ 'Both Changed',
+      insulin_status == 'Steady' & metformin_status == 'Changed' ~ 'Metformin Changed',
+      insulin_status == 'Changed' & metformin_status == 'Steady' ~ 'Insulin Changed',
+      insulin_status == 'Steady' & metformin_status == 'Steady' ~ 'Both Steady',
+    ),
+    instability_tier = factor(
+      instability_tier,
+      levels = c(
+        "Both Changed",
+        "Metformin Changed",
+        "Insulin Changed",
+        "Both Steady"
+      ))) |>
+  group_by(instability_tier) |>
+  ggplot(aes(x = instability_tier,
+             y = readmit_rate,
+             fill = instability_tier)) +
   geom_col(position = "dodge") +
   geom_text(
     aes(label = scales::percent(readmit_rate, accuracy = 0.1)),
     vjust = -0.5,
     size = 4
   ) +
+  scale_fill_manual(values = cols, guide = "none") +
   scale_y_continuous(labels = scales::percent) +
   labs(
-    title = "Readmission Rate by Insulin × Glimepiride Regimen Stability",
-    x = "Insulin and Glimepiride Regimen",
-    y = "30-Day Readmission Rate",
-    fill = "Glimepiride Regimen"
+    title = "Readmission Rate by Metformin x Insulin Regimen Stability",
+    x = "Regimens",
+    y = "30-Day Readmission Rate"
   ) +
   theme_minimal()
+
+ggsave("additional_analyses/plots/insulin_x_metformin_readmit_plot.png", plot = insulin_x_metformin_readmit_plot, width = 6.5, height = 8)
+
+# metformin_glyburide plot
+cols <- c("Both Changed" = "#8DCE8D", "Metformin Changed" = "#4CB04C", "Glyburide Changed" = "#317231", "Both Steady" = "#204D20")
+
+metformin_glyburide <- as.data.frame(interaction_matrices$metformin_glyburide)
+
+glyburide_x_metformin_readmit_plot <- metformin_glyburide |>
+  mutate(
+    instability_tier = case_when(
+      glyburide_status == 'Changed' & metformin_status == 'Changed' ~ 'Both Changed',
+      glyburide_status == 'Steady' & metformin_status == 'Changed' ~ 'Metformin Changed',
+      glyburide_status == 'Changed' & metformin_status == 'Steady' ~ 'Glyburide Changed',
+      glyburide_status == 'Steady' & metformin_status == 'Steady' ~ 'Both Steady',
+    ),
+    instability_tier = factor(
+      instability_tier,
+      levels = c(
+        "Both Changed",
+        "Metformin Changed",
+        "Glyburide Changed",
+        "Both Steady"
+      ))) |>
+  group_by(instability_tier) |>
+  ggplot(aes(x = instability_tier,
+             y = readmit_rate,
+             fill = instability_tier)) +
+  geom_col(position = "dodge") +
+  geom_text(
+    aes(label = scales::percent(readmit_rate, accuracy = 0.1)),
+    vjust = -0.5,
+    size = 4
+  ) +
+  scale_fill_manual(values = cols, guide = "none") +
+  scale_y_continuous(labels = scales::percent) +
+  labs(
+    title = "Readmission Rate by Metformin x Insulin Regimen Stability",
+    x = "Regimens",
+    y = "30-Day Readmission Rate"
+  ) +
+  theme_minimal()
+
+ggsave("additional_analyses/plots/glyburide_x_metformin_readmit_plot.png", plot = glyburide_x_metformin_readmit_plot, width = 6.5, height = 8)
+
+# old plots -----
+# cols <- c("Changed × Changed" = "#8DCE8D", "One Changed" = "#4CB04C", "Steady × Steady" = "#317231")
+# 
+# # insulin x glyburide
+# insulin_x_glyburide_readmit_plot <- diabetic_clean |>
+#   filter(insulin != "No", glyburide!= "No") |>
+#   mutate(
+#     insulin_changed = insulin != "Steady",
+#     glyburide_changed = glyburide != "Steady",
+#     instability_tier = case_when(
+#       !insulin_changed & !glyburide_changed ~ "Steady × Steady",
+#       insulin_changed & glyburide_changed ~ "Changed × Changed",
+#       TRUE ~ "One Changed"
+#     ) ) |> 
+#   group_by(instability_tier) |>
+#   summarize(
+#     readmit_rate = mean(readmitted == "YES", na.rm = TRUE),
+#     n = n(),
+#     .groups = "drop"
+#   ) |> 
+#   ggplot(aes(x = instability_tier,
+#              y = readmit_rate,
+#              fill = instability_tier)) +
+#   geom_col(position = "dodge") +
+#   geom_text(
+#     aes(label = scales::percent(readmit_rate, accuracy = 0.1)),
+#     vjust = -0.5,
+#     size = 4
+#   ) +
+#   scale_fill_manual(values = cols, guide = "none") +
+#   scale_y_continuous(labels = scales::percent) +
+#   labs(
+#     title = "Readmission Rate by Insulin × Glyburide Regimen Stability",
+#     x = "Regimens",
+#     y = "30-Day Readmission Rate"
+#   ) +
+#   theme_minimal()
+# 
+# ggsave("additional_analyses/plots/insulin_x_glyburide_readmit_plot.png", plot = insulin_x_glyburide_readmit_plot, width = 6.5, height = 8)
+# 
+# 
+# # insulin x glimepiride
+# insulin_x_glimepiride_readmit_plot <- diabetic_clean |>
+#   filter(insulin != "No", glimepiride != "No") |>
+#   mutate(
+#     insulin_changed = insulin != "Steady",
+#     glimepiride_changed = glimepiride != "Steady",
+#     instability_tier = case_when(
+#       !insulin_changed & !glimepiride_changed ~ "Steady × Steady",
+#       insulin_changed & glimepiride_changed ~ "Changed × Changed",
+#       TRUE ~ "One Changed"
+#     ) ) |> 
+#   group_by(instability_tier) |>
+#   summarize(
+#     readmit_rate = mean(readmitted == "YES", na.rm = TRUE),
+#     n = n(),
+#     .groups = "drop"
+#   ) |> 
+#   ggplot(aes(x = instability_tier,
+#              y = readmit_rate,
+#              fill = instability_tier)) +
+#   geom_col(position = "dodge") +
+#   geom_text(
+#     aes(label = scales::percent(readmit_rate, accuracy = 0.1)),
+#     vjust = -0.5,
+#     size = 4
+#   ) +
+#   scale_fill_manual(values = cols, guide = "none") +
+#   scale_y_continuous(labels = scales::percent) +
+#   labs(
+#     title = "Readmission Rate by Insulin × Glimepiride Regimen Stability",
+#     x = "Regimens",
+#     y = "30-Day Readmission Rate"
+#   ) +
+#   theme_minimal()
+# 
+# ggsave("additional_analyses/plots/insulin_x_glimepiride_readmit_plot.png", plot = insulin_x_glimepiride_readmit_plot, width = 6.5, height = 8)
+# 
+# # insulin x metformin
+# insulin_x_metformin_readmit_plot <- diabetic_clean |>
+#   filter(insulin != "No", metformin != "No") |>
+#   mutate(
+#     insulin_changed = insulin != "Steady",
+#     metformin_changed = metformin != "Steady",
+#     instability_tier = case_when(
+#       !insulin_changed & !metformin_changed ~ "Steady × Steady",
+#       insulin_changed & metformin_changed ~ "Changed × Changed",
+#       TRUE ~ "One Changed"
+#     ) ) |> 
+#   group_by(instability_tier) |>
+#   summarize(
+#     readmit_rate = mean(readmitted == "YES", na.rm = TRUE),
+#     n = n(),
+#     .groups = "drop"
+#   ) |> 
+#   ggplot(aes(x = instability_tier,
+#              y = readmit_rate,
+#              fill = instability_tier)) +
+#   geom_col(position = "dodge") +
+#   geom_text(
+#     aes(label = scales::percent(readmit_rate, accuracy = 0.1)),
+#     vjust = -0.5,
+#     size = 4
+#   ) +
+#   scale_fill_manual(values = cols, guide = "none") +
+#   scale_y_continuous(labels = scales::percent) +
+#   labs(
+#     title = "Readmission Rate by Insulin × Metformin Regimen Stability",
+#     x = "Regimens",
+#     y = "30-Day Readmission Rate"
+#   ) +
+#   theme_minimal()
+# 
+# ggsave("additional_analyses/plots/insulin_x_metformin_readmit_plot.png", plot = insulin_x_metformin_readmit_plot, width = 6.5, height = 8)
+# 
+# 
+# # metformin x glyburide
+# metformin_x_glyburide_readmit_plot <- diabetic_clean |>
+#   filter(glyburide != "No", metformin != "No") |>
+#   mutate(
+#     glyburide_changed = glyburide != "Steady",
+#     metformin_changed = metformin != "Steady",
+#     instability_tier = case_when(
+#       !glyburide_changed & !metformin_changed ~ "Steady × Steady",
+#       glyburide_changed & metformin_changed ~ "Changed × Changed",
+#       TRUE ~ "One Changed"
+#     ) ) |> 
+#   group_by(instability_tier) |>
+#   summarize(
+#     readmit_rate = mean(readmitted == "YES", na.rm = TRUE),
+#     n = n(),
+#     .groups = "drop"
+#   ) |> 
+#   ggplot(aes(x = instability_tier,
+#              y = readmit_rate,
+#              fill = instability_tier)) +
+#   geom_col(position = "dodge") +
+#   geom_text(
+#     aes(label = scales::percent(readmit_rate, accuracy = 0.1)),
+#     vjust = -0.5,
+#     size = 4
+#   ) +
+#   scale_fill_manual(values = cols, guide = "none") +
+#   scale_y_continuous(labels = scales::percent) +
+#   labs(
+#     title = "Readmission Rate by Glyburide × Metformin Regimen Stability",
+#     x = "Regimens",
+#     y = "30-Day Readmission Rate"
+#   ) +
+#   theme_minimal()
+# 
+# ggsave("additional_analyses/plots/metformin_x_glyburide_readmit_plot.png", plot = metformin_x_glyburide_readmit_plot, width = 6.5, height = 8)
+
+
+# diabetic_clean |>
+#   filter(insulin != "No", glimepiride != "No") |>
+#   mutate(
+#     insulin_collapsed = if_else(insulin == "Steady", "Steady", "Changed"),
+#     glimepiride_collapsed = if_else(glimepiride == "Steady", "Steady", "Changed")
+#   ) |> 
+#   group_by(insulin_collapsed, glimepiride_collapsed) |>
+#   summarize(
+#     readmit_rate = mean(readmitted == "YES", na.rm = TRUE),
+#     n = n(),
+#     .groups = "drop"
+#   ) |> 
+#   filter(insulin_collapsed == 'Changed' & glimepiride_collapsed == 'Changed' | insulin_collapsed == 'Steady' & glimepiride_collapsed == 'Steady') |> 
+#   ggplot( aes(x = insulin_collapsed,
+#               y = readmit_rate)) +
+#   geom_col(position = "dodge") +
+#   geom_text(
+#     aes(label = scales::percent(readmit_rate, accuracy = 0.1)),
+#     vjust = -0.5,
+#     size = 4
+#   ) +
+#   scale_y_continuous(labels = scales::percent) +
+#   labs(
+#     title = "Readmission Rate by Insulin × Glimepiride Regimen Stability",
+#     x = "Insulin and Glimepiride Regimen",
+#     y = "30-Day Readmission Rate",
+#     fill = "Glimepiride Regimen"
+#   ) +
+#   theme_minimal()
+
 
 
 
@@ -701,9 +798,6 @@ flagged_meds <- unique(c(
   double_med_red_flags$medication_1,
   double_med_red_flags$medication_2
 ))
-
-
- 
 
 
 patient_double_long <- diabetic_clean |>
